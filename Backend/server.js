efda1e9363a69c7919eada43323089aa5e40ae06
Vivekkk1829 <vivekkatta1829@gmail.com>
2025-12-18 16:37:15 +0530

    Bug Fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,39 @@
-require("dotenv").config();   
-const express = require('express');
-const app =express();
-const mongoose= require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 app.use(cookieParser());
 
-
-const authRouter=require('./routes/auth-route.js')
-const summariseRouter=require('./routes/summarise-route.js')
+const authRouter = require("./routes/auth-route.js");
+const summariseRouter = require("./routes/summarise-route.js");
 
 //Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
- console.log("Connected to MongoDB");
-})
-.catch((err)=>console.log(err));
-app.use(cors({
-     origin: "http://localhost:5173",
-    credentials: true
-}));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
+app.use(
+  cors({
+    origin: "https://text-summary-6plv.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-app.use('/api/auth',authRouter);
-app.use('/api/summarise',summariseRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/summarise", summariseRouter);
 
-
-
-const port=3000;
-app.listen(port,()=>{
-    console.log(`App is tunning sucessfully on ${port}`);
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App is tunning sucessfully on ${port}`);
+});
